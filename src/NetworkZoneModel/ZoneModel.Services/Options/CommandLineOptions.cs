df7ca12c8b5f5d6cbc5ad9953265ceb47dd7f945
Jordan Knight <jakkaj@gmail.com>
2018-10-10 10:24:10 -0700

    Some tests for the command line parser basics

---
+++ src/NetworkZoneModel/ZoneModel.Services/Options/CommandLineOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace ZoneModel.Services.Options
+{
+    public class CommandLineOptions
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+    }
+}
--- src/NetworkZoneModel/ZoneModel.Tests/Tests/ArgsTests.cs
+++ src/NetworkZoneModel/ZoneModel.Tests/Tests/ArgsTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using CommandLine;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZoneModel.Services.Options;
 
 namespace ZoneModel.Tests.Tests
 {
@@ -9,9 +12,101 @@ namespace ZoneModel.Tests.Tests
     public class ArgsTests : TestBase
     {
         [TestMethod]
-        public void TestBasicArgs()
+        public void TestVerboseArgs()
         {
+            var args = new List<string>();
+            args.Add("--verbose");
 
+            var msr = new ManualResetEvent(false);
+
+            var signalled = false;
+
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed<CommandLineOptions>(o =>
+                {
+                    if (o.Verbose)
+                    {
+                        signalled = true;
+                        msr.Set();
+                        Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example!");
+                    }
+                });
+
+            msr.WaitOne(TimeSpan.FromSeconds(2));
+
+            Assert.IsTrue(signalled);
+        }
+
+        [TestMethod]
+        public void TestBasicVerboseArgs()
+        {
+            var args = new List<string>();
+            args.Add("-v");
+
+            var msr = new ManualResetEvent(false);
+
+            var signalled = false;
+
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed<CommandLineOptions>(o =>
+                {
+                    if (o.Verbose)
+                    {
+                        signalled = true;
+                        msr.Set();
+                        Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example!");
+                    }
+                });
+
+            msr.WaitOne(TimeSpan.FromSeconds(2));
+
+            Assert.IsTrue(signalled);
+        }
+
+
+        [TestMethod]
+        public void TestNoVerboseArgs()
+        {
+            var args = new List<string>();
+            args.Add("");
+
+            var msr = new ManualResetEvent(false);
+
+            var signalled = false;
+
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed<CommandLineOptions>(o =>
+                {
+                    if (o.Verbose)
+                    {
+                        
+                        Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    }
+                    else
+                    {
+                        signalled = true;
+                        msr.Set();
+                        Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example!");
+                    }
+                });
+
+            msr.WaitOne(TimeSpan.FromSeconds(2));
+
+            Assert.IsTrue(signalled);
         }
     }
 }
