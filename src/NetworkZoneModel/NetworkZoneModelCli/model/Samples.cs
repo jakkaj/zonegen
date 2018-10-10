3bcc8e920703084eddd0ebaa4249e185ef64a781
Rian Finnegan <flanagan89@gmail.com>
2018-10-11 04:31:41 +1100

    write out to some files and async main

---
+++ src/NetworkZoneModel/NetworkZoneModelCli/model/Samples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkZoneModelCli
+{
+    // this class contains samples used to create new yaml files
+    public static class Samples
+    {
+        public static NetworkRule NetworkRuleSample(string id) => new NetworkRule() {
+            From = "from-this-zone",
+            To = "to-this-zone",
+            Id = id,
+            IsBidirectional = true,
+            Ports = new List<int> { 22, 80 }
+        };
+
+        public static Zone ZoneExample(string id) => new Zone()
+        {
+            Id = id
+        };
+    }
+}
